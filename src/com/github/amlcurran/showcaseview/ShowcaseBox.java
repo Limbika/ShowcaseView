76cfe28f6d1f98ecea8dd2be199650d7934da0b6
Ruben <moraleshuerva.ruben@gmail.com>
2014-09-15 17:10:45 +0200

    Add option, title with string and description with Charsequence

--- src/com/github/amlcurran/showcaseview/ShowcaseBox.java
+++ src/com/github/amlcurran/showcaseview/ShowcaseBox.java
@@ -89,6 +89,40 @@ public class ShowcaseBox {
 	}
 	
 	/**
+	 * Add new showcase.
+	 * @param target The target view to focus. If is null, focus nothing.
+	 * @param title The title resource. If is 0, without title. 
+	 * @param icon The drawable resource. It will be beside the title. If is 0, without icon.
+	 * @param description The description resource. If is 0, without description. 
+	 * @param image The drawable resource. It will be below the description. If is 0, without image.
+	 * @param touchMode. The touch mode behaviour. Use:
+	 *  - ShowcaseView.TOUCH_ALL You can click in all screen.
+	 *  - ShowcaseView.TOUCH_TARGET You can click only in target view.
+	 *  - ShowcaseView.TOUCH_NONE You cannot click on the screen.
+	 */
+	public void addShowCase(View target, int title, int icon, CharSequence description, int image, int touchMode) {
+		ShowcaseInfo info = new ShowcaseInfo(target, resToCharSequence(title), icon, description, image, touchMode);
+		mShowcaseInfos.add(info);
+	}
+	
+	/**
+	 * Add new showcase.
+	 * @param target The target view id to focus. If is 0, focus nothing.
+	 * @param title The title resource. If is 0, without title. 
+	 * @param icon The drawable resource. It will be beside the title. If is 0, without icon.
+	 * @param description The description resource. If is 0, without description. 
+	 * @param image The drawable resource. It will be below the description. If is 0, without image.
+	 * @param touchMode. The touch mode behaviour. Use:
+	 *  - ShowcaseView.TOUCH_ALL You can click in all screen.
+	 *  - ShowcaseView.TOUCH_TARGET You can click only in target view.
+	 *  - ShowcaseView.TOUCH_NONE You cannot click on the screen.
+	 */
+	public void addShowCase(int target, int title, int icon, CharSequence description, int image, int touchMode) {
+		ShowcaseInfo info = new ShowcaseInfo(target, resToCharSequence(title), icon, description, image, touchMode);
+		mShowcaseInfos.add(info);
+	}
+	
+	/**
 	 * Show the showcases in single shot mode.
 	 */
 	public void show() {
