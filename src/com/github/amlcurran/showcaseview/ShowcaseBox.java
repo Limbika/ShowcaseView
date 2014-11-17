4f67d3b22ccbb96b6306061f999059bd0e59797e
Ruben <moraleshuerva.ruben@gmail.com>
2014-11-17 13:20:52 +0100

    Better javadoc.

--- src/com/github/amlcurran/showcaseview/ShowcaseBox.java
+++ src/com/github/amlcurran/showcaseview/ShowcaseBox.java
@@ -30,10 +30,10 @@ public class ShowcaseBox {
 	/**
 	 * Add new showcase.
 	 * @param target The target view to focus. If is null, focus nothing.
-	 * @param title The title resource. If is 0, without title. 
-	 * @param icon The drawable resource. It will be beside the title. If is 0, without icon.
-	 * @param description The description resource. If is 0, without description. 
-	 * @param image The drawable resource. It will be below the description. If is 0, without image.
+	 * @param title The title resource. If is "", without title. 
+	 * @param icon The drawable resource. It will be beside the title. If is -1, without icon.
+	 * @param description The description resource. If is "", without description. 
+	 * @param image The drawable resource. It will be below the description. If is -1, without image.
 	 * @param touchMode. The touch mode behaviour. Use:
 	 * <ul>
 	 * 	<li>{@link ShowcaseView#TOUCH_ALL} You can click in all screen.</li>
@@ -49,11 +49,11 @@ public class ShowcaseBox {
 	
 	/**
 	 * Add new showcase.
-	 * @param target The target view id to focus. If is 0, focus nothing.
-	 * @param title The title resource. If is 0, without title. 
-	 * @param icon The drawable resource. It will be beside the title. If is 0, without icon.
-	 * @param description The description resource. If is 0, without description. 
-	 * @param image The drawable resource. It will be below the description. If is 0, without image.
+	 * @param target The target view id to focus. If is -1, focus nothing.
+	  * @param title The title resource. If is "", without title. 
+	 * @param icon The drawable resource. It will be beside the title. If is -1, without icon.
+	 * @param description The description resource. If is "", without description. 
+	 * @param image The drawable resource. It will be below the description. If is -1, without image.
 	 * @param touchMode. The touch mode behaviour. Use:
 	 * <ul>
 	 *  <li>{@link ShowcaseView#TOUCH_ALL} You can click in all screen.</li>
